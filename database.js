eca088e9db482f6e667b81d7af199989c04c6528
exizproyect <150602922+exizproyect@users.noreply.github.com>
2025-08-12 20:47:55 -0500

    Update database.js

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-let url = "mongodb+srv://existenzdev:existenzdev@cluster0.isvq3rw.mongodb.net/l?retryWrites=true&w=majority"
+let url = ""
 mongoose.connect(url).then(() => {
  console.log("[ON] > Base de datos conectado")
 }).catch((err) => {
